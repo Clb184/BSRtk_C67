aa36ba87539b455c6c493f9ad716bf13777314e2
Clb184 <84605945+Clb184@users.noreply.github.com>
2024-12-08 12:35:24 -0500

    Add missing -1 returns when fail, add info for sound def compiling in PrintUsage();

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ void PrintUsage(const char* exe) {
 	"	ed file.edt (Dump EDT)\n"
 	"	ec source output.edt <- in pairs (Compile EDT)\n"
 	"	sd sound.def (Dump Sound defs)\n"
+	"	sc input.json output.def (Dump Sound defs)\n"
 		, exe);
 }
 
@@ -19,14 +20,17 @@ int main(int argc, char** argv) {
 	if (op == "ed") {
 		int num_dec = (argc - 2);
 		DumpEDT(argv[2]);
+		return -1;
 	}
 	else if (op == "ec" && !((argc-2) % 2)) {
 		EDTCompiler compiler;
 		int num_comp = (argc - 2) / 2;
 		for (int i = 0; i < num_comp; i++) {
 			compiler.Reset();
-			if (!compiler.CompileEDT(argv[2 + i * 2], argv[2 + i * 2 + 1]))
+			if (!compiler.CompileEDT(argv[2 + i * 2], argv[2 + i * 2 + 1])) {
 				printf("Failed compiling EDT %s.\n", argv[2 + i * 2]);
+				return -1;
+			}
 		}
 	}
 	else if (op == "sd") {
@@ -35,11 +39,14 @@ int main(int argc, char** argv) {
 	else if (op == "sc" && !((argc - 2) % 2)) {
 		int num_comp = (argc - 2) / 2;
 		for (int i = 0; i < num_comp; i++) {
-			if (!CompileSoundDef(argv[2 + i * 2], argv[2 + i * 2 + 1]))
+			if (!CompileSoundDef(argv[2 + i * 2], argv[2 + i * 2 + 1])) {
 				printf("Failed compiling SoundDef %s.\n", argv[2 + i * 2]);
+				return -1;
+			}
 		}
 	}
 	else {
 		PrintUsage(argv[0]);
 	}
+	return 0;
 }
